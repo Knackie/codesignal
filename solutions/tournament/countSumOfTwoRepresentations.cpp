99c3ece2f71232f36b6469600ad1264b2922dad4
Knackie <43431712+Knackie@users.noreply.github.com>
2018-11-09 15:13:22 +0100

    Create countSumOfTwoRepresentations.cpp

---
+++ solutions/tournament/countSumOfTwoRepresentations.cpp
@@ -0,0 +1,12 @@
+int countSumOfTwoRepresentations(int n, int l, int r) {
+ int result = 0;
+
+    for (int a = l; a <= r; a++) {
+        int b = n - a;
+        if (b >= l && b <= r && b >= a) {
+            result++;
+        }
+    }
+    return result;
+}
+
