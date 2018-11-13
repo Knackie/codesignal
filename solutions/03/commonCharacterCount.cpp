fb54faaebd3d8902c6593bd0a9b2013c98ef39c4
Knackie <43431712+Knackie@users.noreply.github.com>
2018-11-13 11:38:13 +0100

    Create commonCharacterCount.cpp

---
+++ solutions/03/commonCharacterCount.cpp
@@ -0,0 +1,13 @@
+int commonCharacterCount(std::string s1, std::string s2) {
+    int count1[26] = {0};
+    int count2[26] = {0};
+    
+    for(char c: s1) ++count1[c-'a'];
+    for(char c: s2) ++count2[c-'a'];
+    
+    int ret = 0;
+    for(int i=0; i<26; ++i)
+        ret += min(count1[i],count2[i]);
+    
+    return ret;
+}
